73c68b6931a6115336f4ef9d9bb634d3efe72bca
Nick <unick.zhow@gmail.com>
2018-08-20 22:18:57 +0800

    save username in store

--- src/store.js
+++ src/store.js
@@ -4,13 +4,15 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+	state: {
+		userName: ''
+	},
+	mutations: {
+		saveUserName (state, payload) {
+			state.userName = payload
+		}
+	},
+	actions: {
 
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+	}
 })
--- src/views/Entry.vue
+++ src/views/Entry.vue
@@ -3,19 +3,26 @@
 		<form class='user-name-form'>
 			<label>請輸入使用者名稱</label>
 			<input type='text' v-model='userName'>
-			<button>確認</button>
+			<button @click.prevent='saveUserName'>確認</button>
 		</form>
 	</main>
 </template>
 
 <script>
-// @ is an alias to /src
 export default {
 	name: 'entry',
-	data() {
+	data () {
 		return {
 			userName: ''
 		}
 	},
+	methods: {
+		saveUserName () {
+			if (this.userName) {
+				this.$store.commit('saveUserName', this.userName)
+				this.$router.push('/home')
+			}
+		}
+	}
 }
 </script>
