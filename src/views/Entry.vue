c6cd8342701ba2db4c198b245ddd6da507661603
Nick <unick.zhow@gmail.com>
2018-08-20 22:07:07 +0800

    entry view UI

--- src/views/Entry.vue
+++ src/views/Entry.vue
@@ -1,11 +1,21 @@
 <template>
-	<main id="entry">
+	<main id='entry'>
+		<form class='user-name-form'>
+			<label>請輸入使用者名稱</label>
+			<input type='text' v-model='userName'>
+			<button>確認</button>
+		</form>
 	</main>
 </template>
 
 <script>
 // @ is an alias to /src
 export default {
-	name: 'entry'
+	name: 'entry',
+	data() {
+		return {
+			userName: ''
+		}
+	},
 }
 </script>
