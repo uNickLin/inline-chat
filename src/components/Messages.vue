e52b248fb06f1f834aa4cbeba15098d324f94eda
Nick <unick.zhow@gmail.com>
2018-08-21 10:16:17 +0800

    messages component

---
+++ src/components/Messages.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="messages">
+		<template v-for='(msg, index) in msgList'>
+			<div class="message others" :key='index' v-if='msg.userName !== myName'>
+				<label>{{msg.userName}} : </label>
+				<p>{{msg.message}}</p>
+			</div>
+			<div class="message mine" :key='index' v-else>
+				<p>{{msg.message}}</p>
+			</div>
+		</template>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'messages',
+	props: ['msgList'],
+	computed: {
+		myName () {
+			return this.$store.state.userName
+		}
+	}
+}
+</script>
